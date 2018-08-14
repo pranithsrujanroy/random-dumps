9ff59c32ce4cf0e951a4e884437354272e87aab8
Pranith Srujan Roy <ammumaddineni97@gmail.com>
2018-08-14 15:46:57 +0500

    Add files via upload

---
+++ apriori.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 15 01:44:51 2018
+
+@author: 115cs0246
+"""
+def create_data_set():
+    n = int(input("Enter the number of transactions"))
+    dataset = []
+    for i in range(0,n):
+        l = raw_input().split()
+        dataset.append(l)
+    return dataset
+
+def create_C1(dataset):
+    c1 = []
+    for transaction in dataset:
+        for item in transaction:
+            if not [item] in c1:
+                c1.append([item])
+                
+    c1.sort()
+    return list(map(frozenset, c1))
+    
+def create_L(dataset, c1, min_sup):
+    count_ck = {}
+    for transaction in dataset:
+        for ck in c1:
+            if ck.issubset(transaction):
+                if not ck in count_ck:
+                    count_ck[ck] = 1
+                else:
+                    count_ck[ck] += 1
+    len_dataset = float(len(dataset))
+    L1 = []
+    support_data = {}
+    for key in count_ck:
+        support = count_ck[key]/len_dataset
+        if support >= min_sup:
+            L1.insert(0,key)
+        support_data[key] = support
+    return L1, support_data
+
+def aprioriGen(L, k):
+    retlist = []
+    lenL = len(L)
+    for i in range(lenL):
+        for j in range(i+1, lenL):
+            L1 = list(L[i])[:k-2]
+            L2 = list(L[j])[:k-2]
+            L1.sort()
+            L2.sort()
+            if L1==L2:
+                retlist.append(L[i] | L[j])
+    return retlist
+    
+def apriori(dataset, min_sup=0.5):
+    C1 = create_C1(dataset)
+    d = list(map(set, dataset))
+    L1, support_data = create_L(d, C1, min_sup)
+    L = [L1]
+    k = 2
+    while len(L[k-2])>0:
+        ck = aprioriGen(L[k-2], k)
+        Lk, supk = create_L(d, ck, min_sup)
+        support_data.update(supk)
+        L.append(Lk)
+        k = k+1
+    return L, support_data
+
+dataset = create_data_set()
+L, support_data = apriori(dataset)
+
